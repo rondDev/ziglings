24979e02c80fda5efc9b781a078b36a75a0c301b
rond <contact@rond.cc>
2024-02-07 00:02:59 +0100

    feat: solve 068

--- exercises/068_comptime3.zig
+++ exercises/068_comptime3.zig
@@ -27,8 +27,8 @@ const Schooner = struct {
     bowsprit_length: u32 = 34,
     mainmast_height: u32 = 95,
 
-    fn scaleMe(self: *Schooner, comptime scale: u32) void {
-        comptime var my_scale = scale;
+    fn scaleMe(self: *Schooner, scale: u32) void {
+        var my_scale = scale;
 
         // We did something neat here: we've anticipated the
         // possibility of accidentally attempting to create a
@@ -43,7 +43,7 @@ const Schooner = struct {
         //
         // Please change this so that it sets a 0 scale to 1
         // instead.
-        if (my_scale == 0) @compileError("Scale 1:0 is not valid!");
+        if (my_scale == 0) my_scale = 1;
 
         self.scale = my_scale;
         self.hull_length /= my_scale;
